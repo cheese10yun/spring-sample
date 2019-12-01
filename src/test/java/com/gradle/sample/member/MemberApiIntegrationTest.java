78681b455bf14e13d5dc18063088fd9ad887e567
cheese10yun <cheese10yun@gmail.com>
2019-12-01 19:19:22 +0900

    sample test....

--- src/test/java/com/gradle/sample/member/MemberApiIntegrationTest.java
+++ src/test/java/com/gradle/sample/member/MemberApiIntegrationTest.java
@@ -1,13 +1,8 @@
 package com.gradle.sample.member;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import com.gradle.sample.test.IntegrationApiTest;
 import org.junit.Test;
@@ -27,14 +22,7 @@ public class MemberApiIntegrationTest extends IntegrationApiTest {
 
     //then
     actions
-        .andExpect(status().isOk())
-        .andDo(document(identifier,
-            pathParameters(parameterWithName("id").description("Member Id")),
-            responseFields(
-                fieldWithPath("id").description("The Member's email address"),
-                fieldWithPath("name").description("The Member's address city"),
-                fieldWithPath("email").description("The Member's address street")
-            )));
+        .andExpect(status().isOk());
 
   }
 
--- src/test/java/com/gradle/sample/test/IntegrationApiTest.java
+++ src/test/java/com/gradle/sample/test/IntegrationApiTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -18,19 +17,13 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
-@Import(RestDocsConfiguration.class)
 @ActiveProfiles("test")
 @Transactional
 @Ignore
 public class IntegrationApiTest {
 
-  protected final String identifier = "{class-name}/{method-name}";
   @Autowired
   protected MockMvc mvc;
   @Autowired
   protected ObjectMapper objectMapper;
-  @Autowired
-  protected ResourceLoader resourceLoader;
-
-
 }
